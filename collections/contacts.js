e35c6cb5621fda0918eb3ae6a63409b45198663f
DerrickJames <dmushangi@gmail.com>
2015-09-23 13:59:56 +0300

    Added schema

---
+++ collections/contacts.js
@@ -0,0 +1,19 @@
+ContactsSchema = new SimpleSchema({
+  name: {
+    type: String,
+    min: 3,
+    max: 20
+  },
+  email: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    optional: true
+  },
+  lastUpdated: {
+    type: Date,
+    optional: true
+  }
+});
+
